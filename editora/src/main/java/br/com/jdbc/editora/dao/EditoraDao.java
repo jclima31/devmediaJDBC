a80d078d0aa0cf69010e4b8f17de9bafc6aa3029
Julio Lima <jclima27@yahoo.com.br>
2019-01-25 19:02:53 -0200

    Adicionando outras formas de insert, com retorno de Id e objeto

--- editora/src/main/java/br/com/jdbc/editora/dao/EditoraDao.java
+++ editora/src/main/java/br/com/jdbc/editora/dao/EditoraDao.java
@@ -1,7 +1,11 @@
 package br.com.jdbc.editora.dao;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import br.com.jdbc.editora.model.Editora;
@@ -11,6 +15,29 @@ public class EditoraDao {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	public Editora add(Editora editora){
+		
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		insert.setTableName("EDITORAS");
+		insert.setColumnNames(Arrays.asList("RAZAO_SOCIAL", "CIDADE", "EMAIL"));
+		insert.setGeneratedKeyName("ID_EDITORA");
+		
+		Number key = insert.executeAndReturnKey(new BeanPropertySqlParameterSource(editora));
+		editora.setId(key.intValue());
+		return editora;
+	}
+	
+	public int save(Editora editora){
+		
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		insert.setTableName("EDITORAS");
+		insert.setColumnNames(Arrays.asList("RAZAO_SOCIAL", "CIDADE", "EMAIL"));
+		insert.setGeneratedKeyName("ID_EDITORA");
+		
+		Number key = insert.executeAndReturnKey(new BeanPropertySqlParameterSource(editora));
+		return key.intValue();
+	}
 	
 	public int insert(Editora editora){
 		
