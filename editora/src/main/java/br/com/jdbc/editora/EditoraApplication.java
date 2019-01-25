54b55041f355f5d2a50d7da356737fa9dcf7b753
Julio Lima <jclima27@yahoo.com.br>
2019-01-25 14:26:26 -0200

    Iniciando o projeto

---
+++ editora/src/main/java/br/com/jdbc/editora/EditoraApplication.java
@@ -0,0 +1,14 @@
+package br.com.jdbc.editora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditoraApplication.class, args);
+	}
+
+}
+
